5c3800cf5069b2ac1217df8a6a81802745590e8b
VQHoangViet <nhockpopi6@gmail.com>
2022-11-08 12:04:00 +0700

    5

--- Report_BI_tool_check_POD_version3_1.py
+++ Report_BI_tool_check_POD_version3_1.py
@@ -330,7 +330,10 @@ def read_pipeline(url_agg:str, str_time_from_:str, str_time_to_:str, split_from_
   # else:
   #   sev = pd.DataFrame()
   # clear_output()
-  big_frame = read_folder_pod_resultQA_in_month(str_time_from_, str_time_to_).drop(columns=['final_result', 'corrected_dispute'])
+  try:
+    big_frame = read_folder_pod_resultQA_in_month(str_time_from_, str_time_to_).drop(columns=['final_result', 'corrected_dispute'])
+  except:
+    big_frame = read_folder_pod_resultQA_in_month(str_time_from_, str_time_to_)
   df = pre_processing(big_frame.loc[(pd.to_datetime(big_frame['attempt_date']) >= pd.Timestamp(str_time_from_)) & (pd.to_datetime(big_frame['attempt_date']) <= pd.Timestamp(str_time_to_))])
 
 
