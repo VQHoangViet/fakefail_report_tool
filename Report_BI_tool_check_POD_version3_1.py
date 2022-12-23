324bbf6ddeed109922ecfa9623169b02f57abc9a
VQHoangViet <nhockpopi6@gmail.com>
2022-12-23 12:01:47 +0700

    3

--- Report_BI_tool_check_POD_version3_1.py
+++ Report_BI_tool_check_POD_version3_1.py
@@ -11,7 +11,6 @@ from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from IPython.display import clear_output
 from os import listdir
 from os.path import isfile, join
-import os
 from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -19,8 +18,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from plotly.subplots import make_subplots
-import warnings
-warnings.filterwarnings('ignore')
+
 
 drive.mount('/content/drive', force_remount=True)
 pd.set_option('display.max_columns', None)
