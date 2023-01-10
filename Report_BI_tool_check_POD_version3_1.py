ab65ef2bd90e0461ef6b4247118dee867bbd5c43
VQHoangViet <nhockpopi6@gmail.com>
2023-01-10 14:08:32 +0700

    3

--- Report_BI_tool_check_POD_version3_1.py
+++ Report_BI_tool_check_POD_version3_1.py
@@ -531,9 +531,9 @@ def compute_phase(x):
 # get max attempt date
 def get_attempt_date(x, col_name, max=True):
   if max:
-    return x[col_name].max().to_period('M')
+    return x[col_name].max().strftime('%Y-%M')
   if max==False:
-    return x[col_name].min().to_period('M')
+    return x[col_name].min().strftime('%Y-%M')
 
 
 # Final: exporting
